fb0885ff256e2f5bd0399cfeffe5ae5b2eb8f12d
chenxinsi <547187896@qq.com>
2017-05-31 10:57:22 +0800

    first commit

--- learn/models.py
+++ learn/models.py
@@ -1,3 +1,7 @@
-from django.db import models
 
 # Create your models here.
+from django.db import models
+
+class Test(models.Model):
+    name = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
--- learn/views.py
+++ learn/views.py
@@ -4,7 +4,7 @@
 ##def index(request):
 ##    return HttpResponse(u"welcome!")
 from django.shortcuts import render
- 
+from learn.models import Test 
  
 def home(request):
     string = u"心思的个人网页"
@@ -12,4 +12,6 @@ def home(request):
     info_dict = {'name1': u'张三', 'name2': u'李四'}
    # return render(request, 'home.html', {'TutorialList': TutorialList})
     #return render(request, 'home.html', {'info_dict': info_dict})
-    return render(request, 'index.html')
+    names = Test.objects.all() #查询所有names
+    print (names)
+    return render(request, 'index.html', {'names': names})
